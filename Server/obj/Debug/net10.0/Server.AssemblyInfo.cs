dae72bebc83713805b5d11af4da97e69674fada9
knshielim <keishanalanishielim@gmail.com>
2026-06-30 22:52:03 +0800

    Fix Bug

--- Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4030a628ee9c4fdf5dea3833c0a6903e0ac9bd9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28354073aff5c231ae1bd6a81ec1c671fab15bc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AIAssistantChat from "./components/AIAssistantChat";
 import "./index.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/assistant" element={<AIAssistantChat />} />
-        {/* teammates add their own routes here, e.g. /recipes, /meal-plan, /dashboard */}
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <AIAssistantChat />
+    </div>
   );
 }
 
